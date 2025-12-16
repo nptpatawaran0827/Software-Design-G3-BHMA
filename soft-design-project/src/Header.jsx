5bb65a15ffe2a07a7c37329dc1d4a40ca0622a28
Nheil Patrick Patawaran <nheilpatrick33@gmail.com>
2025-12-16 15:30:33 +0800

    Project Draft

---
+++ soft-design-project/src/Header.jsx
@@ -0,0 +1,20 @@
+
+function Header (){
+    return(
+        <header>
+            <h1>Health Monitoring System</h1>
+            <div>
+                <nav>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Records</a></li>
+                        <li><a href="#">Analytics</a></li>
+                        <li><a href="#">About</a></li>
+                    </ul>
+                </nav>
+            </div> 
+        </header>
+    );
+}
+
+export default Header
