c8f1ffcfd19b7166d0b4fbd7acf84cba6b529280
Nheil Patrick Patawaran <nheilpatrick33@gmail.com>
2025-12-22 00:38:56 +0800

    Updated the Login page

--- soft-design-project/src/Header.jsx
+++ soft-design-project/src/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <div className="p-3 mb-4 rounded-4 shadow-sm text-white d-flex align-items-center" 
          style={{ backgroundColor: '#4da3ff', height: '80px', margin: '20px' }}>
       <h2 className="m-0 fw-bold tracking-wider px-3" style={{ letterSpacing: '2px' }}>
-        BRGY. HEALTH RECORD SYSTEM
+        BARANGAY HEALTH RECORDS and ANALYTICS SYSTEM
       </h2>
     </div>
   );
