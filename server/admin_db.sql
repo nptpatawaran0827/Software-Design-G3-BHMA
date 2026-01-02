48c1c9d59e9a996172fd676b86bc3de037a5afab
Jianvench <jianpalculan18@gmail.com>
2026-01-02 20:13:43 +0800

    Healthform to DB

--- server/admin_db.sql
+++ server/admin_db.sql
@@ -42,6 +42,48 @@ LOCK TABLES `admins` WRITE;
 INSERT INTO `admins` VALUES (1,'Admin_H1','34f26e6e8d36cedafae5617bbd69f40963f12986b916e926a4afd5f7567a6707'),(3,'Admin_H2','63537d7fea69535a6abbba097662d374870890f6814de11c840bc7e7d0ffe91d');
 /*!40000 ALTER TABLE `admins` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `health_records`
+--
+
+DROP TABLE IF EXISTS `health_records`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `health_records` (
+  `Health_Record_ID` int NOT NULL AUTO_INCREMENT,
+  `Resident_Name` varchar(255) NOT NULL,
+  `Resident_ID` varchar(50) NOT NULL,
+  `Age` int DEFAULT NULL,
+  `Sex` enum('Male','Female') DEFAULT NULL,
+  `Civil_Status` varchar(50) DEFAULT NULL,
+  `Blood_Pressure` varchar(20) DEFAULT NULL,
+  `Weight` decimal(5,2) DEFAULT NULL,
+  `Height` decimal(5,2) DEFAULT NULL,
+  `BMI` decimal(4,2) DEFAULT NULL,
+  `Health_Condition` varchar(100) DEFAULT NULL,
+  `Diagnosis` text,
+  `Allergies` text,
+  `Contact_Number` varchar(20) DEFAULT NULL,
+  `Address` text,
+  `Date_Visited` date DEFAULT NULL,
+  `Date_Registered` datetime DEFAULT CURRENT_TIMESTAMP,
+  `status` enum('Active','Not Active') DEFAULT 'Active',
+  `Remarks` text,
+  `updatedAt` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`Health_Record_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `health_records`
+--
+
+LOCK TABLES `health_records` WRITE;
+/*!40000 ALTER TABLE `health_records` DISABLE KEYS */;
+INSERT INTO `health_records` VALUES (2,'Ikki Dominique Modar','01',21,'Female','Single','120/80',62.00,160.00,60.00,'Tonsilitis','May nana lalamunan','Manok','09156866402','De Castro, Pasig','2026-01-02','2026-01-02 20:01:45','Active','Sundot pa more','2026-01-02 20:01:45');
+/*!40000 ALTER TABLE `health_records` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -52,4 +94,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-01-02  0:28:09
+-- Dump completed on 2026-01-02 20:05:06
