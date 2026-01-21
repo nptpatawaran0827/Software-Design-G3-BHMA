58e489a1f56eec3824250c47f58499963953025e
Nheil Patrick Patawaran <nheilpatrick33@gmail.com>
2026-01-22 02:02:07 +0800

    updated the credential of mysql

--- server/server.js
+++ server/server.js
@@ -10,9 +10,9 @@ app.use(express.json());
 
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '@Group3-BHMA',
+  host: 'health-monitoring-analytics-system.colao00gscmx.us-east-1.rds.amazonaws.com',
+  user: 'admin', // or your RDS username
+  password: 'Group3-BHMA', // replace with your RDS password
   database: 'admin_db'
 });
 
