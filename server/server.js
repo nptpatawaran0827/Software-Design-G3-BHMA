6e39e7c67f74767eae6a6bcb32c3e1f049898ffd
Nheil Patrick Patawaran <nheilpatrick33@gmail.com>
2026-01-25 15:51:43 +0800

    latest update

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Group3-BHMA",
+  password: "@Group3-BHMA",
   database: "admin_db",
 });
 
